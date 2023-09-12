5b9f1bafdccdf078311f099656d64677253add28
wenlong.yu <13636488415@163.com>
2023-09-12 10:44:36 +0800

    242.有效的字母异位词、349.两个数组的交集

---
+++ src/main/java/com/practice/day05/IntersectionOfTwoArrays.java
@@ -0,0 +1,31 @@
+package com.practice.day05;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class IntersectionOfTwoArrays {
+
+    // 遍历num1, 定义一个set1存储nums1；遍历nums2, 定义set2存储交集元素，最后将set2转换为int数组返回
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        for (int num1 : nums1) {
+            set1.add(num1);
+        }
+
+        for (int num2 : nums2) {
+            if (set1.contains(num2)) {
+                set2.add(num2);
+            }
+        }
+
+        int[] res = new int[set2.size()];
+        int i = 0;
+        for (Integer item : set2) {
+            res[i++] = item;
+        }
+        return res;
+    }
+}
+
---
+++ src/main/java/com/practice/day05/ValidAnagram.java
@@ -0,0 +1,55 @@
+package com.practice.day05;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidAnagram {
+
+    // 使用hashmap，key表示字母，value表示次数
+    // 首先遍历字符串 s存储字母元素并计数
+    // 然后遍历字符串 t如果不存在相应字母则返回false，反之给字母相应的计数减1，如果计数为0则删除元素
+    // 最后如果map为空则互为字母异位词
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            Integer count = map.getOrDefault(ch, 0);
+            map.put(ch, ++count);
+        }
+
+        for (int j = 0; j < t.length(); j++) {
+            char ch = t.charAt(j);
+            if (map.containsKey(ch)) {
+                Integer count = map.get(ch);
+                map.put(ch, --count);
+                if (count == 0) {
+                    map.remove(ch);
+                }
+            } else {
+                return false;
+            }
+        }
+        return map.size() == 0;
+    }
+
+    // 使用int数组，数组下标0-25代表a-z，数组的值代表次数
+    public boolean isAnagram2(String s, String t) {
+        int[] arr = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - 'a'] += 1;
+        }
+
+        for (int j = 0; j < t.length(); j++) {
+            arr[t.charAt(j) - 'a'] -= 1;
+        }
+
+        for(int item : arr) {
+            if(item != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
